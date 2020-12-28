8efdb2a2ec73386e60ebb661b99e1b663c51f787
MTM828 <71281115+MTM828@users.noreply.github.com>
2020-12-28 22:09:55 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
+//u can delete these comments, they explain the code
 function toggleDarkMode() {
-    elms = document.querySelectorAll("*");
-    for(i=0; i<elms.length; i++) {
-        elms[i].classList.toggle("darkMode");
+    elms = document.querySelectorAll("*");//queryselector all returns a list of all the elements with that class/that name.
+    for(i=0; i<elms.length; i++) {//well loop through each element
+        elms[i].classList.toggle("darkMode");//and toggle the darkMode class.
     }
 }
